e7db994ad0dd26fdc5e0de63e2b2bf0979e536d1
sameer das <sameerdas6143@gmail.com>
2025-04-30 19:04:32 +0530

    initial commit

---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,26 @@
+import Header from "../component/Header"
+import JobSearchBar from "../component/JobSearchinput"
+
+
+
+const Homepage=()=>{
+       return(
+        <div className="font-[Poppins]" >
+                <Header/>
+                 <main>
+                     <JobSearchBar/>
+                 </main>
+                <section>
+                
+                </section>
+
+                <footer>
+                
+                </footer>
+
+        </div>
+       )
+}
+
+
+export default Homepage
