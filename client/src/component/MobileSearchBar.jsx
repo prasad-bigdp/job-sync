331017a1d6b7e12c4566c7b20747e269c514577f
Das Sameer <112536688+Sameer069@users.noreply.github.com>
2025-04-30 19:37:17 +0530

    Update MobileSearchBar.jsx

--- client/src/component/MobileSearchBar.jsx
+++ client/src/component/MobileSearchBar.jsx
@@ -7,7 +7,7 @@ const MobileSearchBar=()=>{
         <div className=' max-lg:block w-full hidden cursor-pointer'>
         <div className="border rounded-[20px] max-lg:w-full p-3 flex items-center h-[50px] border-stone-300  shadow-lg">
         <Search color='gray'/>
-        <span className='w-full px-3 text-[13px] inline-block'>Over 8,00,000 openings delivered perfectly</span>
+        <span className='w-full px-3 text-[13px] inline-block'>Over 8,00,000 openings delivered perfectly.</span>
      
         </div>
       </div>
