7e4a3c329d955b316fc256d8f59f1f6b69dd8776
prasad-bigdp <bigdp585@gmail.com>
2025-04-30 14:59:33 +0530

    job post form group2 ui and basic route completed

--- client/src/pages/Jobs/PostJob.jsx
+++ client/src/pages/Jobs/PostJob.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import axios from "axios"
 import { useAuth } from "../context/AuthContext";
+  // this is basic post job
 
 const { auth } = useAuth();
 
@@ -19,7 +20,6 @@ const PostJob = () => {
 		const { name, value } = e.target
 		setFormData((prev) => ({ ...prev, [name]: value }))
 	}
-
 	const handleSubmit = async (e) => {
 		e.preventDefault()
 
