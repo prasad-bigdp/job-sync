0147bb4a0e84dc045c96f3eee6e21f0d56f0bd3b
Ronak218 <jyotiadityahota@gmail.com>
2025-05-02 15:33:10 +0530

    Job Matching Logic Done

--- server/routes/job.js
+++ server/routes/job.js
@@ -1,8 +1,9 @@
-// Job Matching Route : 
 const express = require("express")
 const router = express.Router()
-const auth = require("../middleware/auth")
+const auth = require("../middleware/authMiddleware")
 const jobController = require("../controllers/jobController")
+
+//added route path for job matching :
 
 router.get("/match" , auth , jobController.getMatchedJobs)
 
