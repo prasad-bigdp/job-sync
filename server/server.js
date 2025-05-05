a1759c5f38e4225f7ba256f7257e1fa0da7c3284
Aayush Sahu <sahuaayush2999@gmail.com>
2025-05-05 18:16:50 +0530

    Apply Section back-end code

--- server/server.js
+++ server/server.js
@@ -9,11 +9,24 @@ connectDB()
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
+<<<<<<< Updated upstream
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/users", require("./routes/user"))
 app.use("/api/employers", require("./routes/employer"))
 app.use("/api/jobs", require("./routes/job"))
+=======
+// Logger
+const logger = require("morgan");
+app.use(logger("tiny"));
+
+
+// app.use("/api/auth", require("./routes/auth"))
+// app.use("/api/users", require("./routes/user"))
+// app.use("/api/employers", require("./routes/employer"))
+// app.use("/api/jobs", require("./routes/job"))
+>>>>>>> Stashed changes
 app.use("/api/applications", require("./routes/application"))
 
 const PORT = process.env.PORT || 5000
