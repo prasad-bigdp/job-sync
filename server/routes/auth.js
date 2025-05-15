ead551ca1086774f30d2d9821db4d43baa7864bf
KrushnaChandra Nayak <131634927+krushnachandranayak@users.noreply.github.com>
2025-05-15 18:26:18 +0530

    Resolve merge conflict

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-
-router.post('/signup', authController.signUp);
-router.post('/signin', authController.signIn);
-
-module.exports =router
