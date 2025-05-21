a8b4a805b8919e8610ffc33ac84a3a5c501ae073
KrushnaChandra Nayak <131634927+krushnachandranayak@users.noreply.github.com>
2025-05-21 18:49:12 +0530

    Group 7 GET and UPDATE users & emplyoer profiles (Backend)

--- server/routes/employer.js
+++ server/routes/employer.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const employerController = require('../controllers/employerController');
 const authController = require('../controllers/employerAuthController');
 const authMiddleware = require('../middleware/authMiddleware');
-
+const { getEmployerProfile, updateEmployerProfile } = require('../controllers/employerController');
 
 // Public routes
 router.post('/login', authController.loginEmployer);
@@ -13,8 +13,12 @@ router.get('/', employerController.getAllEmployers);
 // Protected route
 router.get('/employer-dashboard', authMiddleware, authController.getEmployerDashboard);
 
-// Dynamic route
-router.get('/:id', employerController.getEmployerById);
+
+// Group-7 profile
+// Get employer profile by ID
+router.get('/:id', getEmployerProfile);
+// Update employer profile
+router.put('/:id', authMiddleware, updateEmployerProfile);
 
 
 module.exports = router;
